b144f04b0af016adbc74387a1edda331f5e82b8a
Anshu Kumar Mandal <anshukumarmandal@Anshus-MacBook-Air.local>
2026-02-12 00:13:05 +0530

    fix: hardcode site url for sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || '',
+  siteUrl: 'https://portfolio-omega-sandy-57.vercel.app', // <-- Yahan maine tera Vercel link daal diya hai
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   robotsTxtOptions: {
